3b974eaf54b6da99424e13d300b6ce274552cd49
Waqar Khan <waqar.khan@img-advisors.com>
2022-09-19 23:12:33 +0500

    google pagination added and json file stored

--- google-scrapper.py
+++ google-scrapper.py
@@ -7,35 +7,45 @@ from webdriver_manager.chrome import ChromeDriverManager
 import csv
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-query = 'site:linkedin.com/in/ AND "PHP developer" AND "Islamabad"'
+query = 'site:linkedin.com/in/ AND "React Native" AND "Islamabad"'
 file_name = 'results_file.csv'
 
-driver.get('https:www.google.com')
-time.sleep(3)
+driver.get('http:www.google.com/')
+time.sleep(4)
 
 search_query = driver.find_element_by_name('q')
 search_query.send_keys(query)
-time.sleep(0.5)
+time.sleep(3)
 
 search_query.send_keys(key.ENTER)
 time.sleep(3)
 
-src = driver.page_source
-  
-# Now using beautiful soup
-soup = BeautifulSoup(src, 'lxml')
+isNextPage = True
+profileArray = []
 
+#nextPageURL = nextpage['href']
 # Extracting the HTML of the complete introduction box
 # that contains the name, company name, and the location
-hrefs =  soup.find_all('a', href=True)
-profileArray = []
-for link in hrefs:
-    if("https://pk.linkedin.com/in/" in link['href']):
-        profilelink = link['href']
-        profileArray.append([profilelink])
-        
-print(profileArray)
-
+while(isNextPage):
+    src = driver.page_source
+    # Now using beautiful soup
+    soup = BeautifulSoup(src, 'lxml')
+    hrefs =  soup.find_all('a', href=True)
+    for link in hrefs:
+        if("https://pk.linkedin.com/in/" in link['href']):
+            profilelink = link['href']
+            profileArray.append([profilelink])
+    print(profileArray)
+    try:
+        nextpage = driver.find_element_by_id("pnnext")
+        nextpage.click()
+    except:
+        print("\nNo More Pages to Scrap\n")
+        nextpage = False
+    if(nextpage):
+        time.sleep(5)
+    else:
+        isNextPage = False
 
 with open('profiles.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
--- profile-scrapper.py
+++ profile-scrapper.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 import csv
+import json
+
+def index_in_list(a_list, index):
+    value = index < len(a_list)
+    return value
+
+
 file = open('profiles.csv')
 
 type(file)
@@ -47,21 +54,80 @@ driver.find_element_by_xpath("//button[@type='submit']").click()
 #  "https://www.linkedin.com/in/zehra-ahmed/"
 
 #  ]
-
+info = []
 for profile_url in profilesArray:
   print(profile_url)
+  profile = dict()
   time.sleep(3)
   driver.get(profile_url)
   src = driver.page_source
   soup = BeautifulSoup(src, 'lxml')
+  try:
+    profilePicHTML = soup.find("img", {'class', "pv-top-card-profile-picture__image pv-top-card-profile-picture__image--show ember-view"})
+    profile_pic = profilePicHTML['src']
+  except:
+    profile_pic = ""
   intro = soup.find('div', {'class': 'ph5'})
-  name_loc = intro.find("h1")
-  name = name_loc.get_text().strip() 
-  works_at_loc = intro.find("div", {'class': 'text-body-medium'})
-  works_at = works_at_loc.get_text().strip()
-  location_loc = intro.find_all("span", {'class': 'text-body-small'})
-  #location = location_loc[2].get_text().strip()
-  print("Name : ", name,
-    "\nWorks At : ", works_at)
+  try: 
+    name_loc = intro.find("h1")
+    name = name_loc.get_text().strip() 
+  except:
+    name = ""
+  
+  try:
+    works_at_loc = intro.find("div", {'class': 'text-body-medium'})
+    works_at = works_at_loc.get_text().strip()
+  except: 
+    works_at = ""
+  try:
+
+    location_loc = intro.find_all("span", {'class': 'text-body-small'})
+    company = ""
+    if(index_in_list(location_loc, 0)): 
+      company = location_loc[0].get_text().strip()
+    
+    university = ""
+    if(index_in_list(location_loc, 1)):
+      university = location_loc[1].get_text().strip()
+    
+    location = ""
+    if(index_in_list(location_loc, 2)):
+      location = location_loc[2].get_text().strip()
+  except:
+    company = ""
+    university = ""
+    location = ""
+  
+  profile['name'] = name
+  profile['url'] = profile_url
+  profile['profile_pic'] = profile_pic
+  profile['designation'] = works_at
+  profile['company'] = company
+  profile['university'] = university
+  profile['location'] = location
+  
+  print(profile)
+  info.append(profile)
+
+print(info)
+
+fieldnames = ['name', 'url', 'profile_pic',  'designation', 'company', 'university',  'location']
+with open('data.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(info)
+
+with open('result.json', 'w') as fp:
+    json.dump(info, fp)
+
+
+driver.close()
+
+
+
+
+
+
+
 
 
